466fa1aa379bfd2d30ddcb9f800610f48fca062f
Bridgit-wairimu <kirikabetty@gmail.com>
2021-06-01 07:09:25 -0700

    did some modification

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 $(document).ready(function(){
 $('h1').click(function(){
     alert("This is a heading tag")
-    alert("I told you this is header")
 });
 
-$('p').click(function(){
+$('#para1').click(function(){
     alert("This is a paragraph")
 });
 
 $('img').click(function(){
-    alert("This ia an image")
+    alert("This is an image")
 });
 });
+
+$(document).ready(function(){
+    $("p").click(function(){
+        $(".image").show();
+    });
+});
+
+$(document).ready(function(){
+    $(".clickable").click(function() {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
+    });
+});
+
